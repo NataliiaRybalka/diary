d7af320156b6f4be1c171e2b85715bf7fac13d7a
NataliiaRybalka <natalirybalka21@gmail.com>
2023-08-07 12:08:15 +0300

    layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,62 @@
 import './App.css';
 
+import { Routes, Route, Outlet, Link } from 'react-router-dom';
+
 import logo from './img/logo.png';
+
+import Registration from './components/user/Registration';
 
 function App() {
 	return (
 		<div className="App">
 			<header>
 				<img src={logo} alt='Dear Diary' className='logo' />
+
+				<div>
+					<nav>
+						<ul>
+							<li className='mainNavLi'><Link to="/">Home</Link></li>
+							<li className='mainNavLi'><Link to="/registration">Registration</Link></li>
+						</ul>
+					</nav>
+
+					<hr />
+					<Outlet />
+				</div>
 			</header>
+
+			<main>
+				<Routes>
+					<Route index element={<Home />} />
+					<Route path="registration" element={<Registration />} />
+				</Routes>
+			</main>
+
+			<footer>
+				<div>
+					<p className={'footerLink'}>
+					<span>
+						&copy; {new Date().getFullYear()}{" "}
+						<a
+						href="https://github.com/NataliiaRybalka"
+						target="blank"
+						>
+						Nataliia Rybalka
+						</a>
+					</span>
+					</p>
+				</div>
+			</footer>
 		</div>
 	);
 }
 
 export default App;
+
+function Home() {
+	return (
+		<div>
+			<h2>Home</h2>
+		</div>
+	);
+}
---
+++ web/src/components/user/Registration.tsx
@@ -0,0 +1,15 @@
+function Registration(): any {
+	return (
+		<div>
+			<h2>Registration</h2>
+
+			<div>
+				<label>Email: </label> <input type='email' />
+				<label>Username: </label> <input type='text' />
+				<label>Password: </label> <input type='password' />
+			</div>
+		</div>
+	);
+}
+
+export default Registration;
