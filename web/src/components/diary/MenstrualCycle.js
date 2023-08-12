bb16eaad6d6fa531e20063faa52f5dd93828d104
NataliiaRybalka <natalirybalka21@gmail.com>
2023-08-12 19:08:58 +0300

    create table

--- web/src/components/diary/MenstrualCycle.js
+++ web/src/components/diary/MenstrualCycle.js
@@ -2,6 +2,8 @@ import { useTranslation } from 'react-i18next';
 
 import Menu from './Menu';
 
+import './MenstrualCycle.css';
+
 function MenstrualCycle() {
 	const { t } = useTranslation();
 
@@ -10,6 +12,22 @@ function MenstrualCycle() {
 			<h1>{t('Menstrual Cycle')}</h1>
 
 			<Menu />
+
+			<table>
+				<thead>
+					<tr>
+						<td>{t('Month')}</td>
+						<td>{t('Start Date')}</td>
+						<td>{t('End Date')}</td>
+						<td>{t('Duration of Menstruation')}</td>
+						<td>{t('Duration of Cycle')}</td>
+						<td>{t('Ovulation')}</td>
+						<td>{t('Emotional State Notes')}</td>
+					</tr>
+				</thead>
+
+				<tbody></tbody>
+			</table>
 		</div>
 	);
 };
