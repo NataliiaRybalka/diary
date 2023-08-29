afe052232bc850dfbac2e21b4778b86a665a0533
NataliiaRybalka <natalirybalka21@gmail.com>
2023-08-29 18:26:45 +0300

    get notifications 10 min less than now

--- server/src/cron/notification.cron.ts
+++ server/src/cron/notification.cron.ts
@@ -1,6 +1,41 @@
 import { CronJob } from 'cron';
 
-export const job = new CronJob('* * * * * *', function() {
-	const d = new Date();
-	console.log('Every second:', d);
+import NotificationSchema from '../db/notification/notification.schema';
+
+// export const job = new CronJob('*/10 * * * *', async () => {
+export const job = new CronJob('* * * * * *', async () => {
+	try {
+		let taskDate = new Date().toLocaleDateString();
+		let time = new Date().toLocaleTimeString('ru');
+		const timeArr = time.split(':') as any[];
+		timeArr[1] = Number(timeArr[1]);
+		if ((timeArr[1] - 10) >= 0) timeArr[1] = timeArr[1] - 10;
+		else {
+			if ((timeArr[0] - 1) >= 0) {
+				timeArr[0] = timeArr[0] - 1;
+				timeArr[1] = 60 + (timeArr[1] - 10);
+			} else {
+				let taskDateArr = taskDate.split('/') as string[];
+				taskDateArr[1] = String(Number(taskDateArr[1]) - 1);
+				taskDate = taskDateArr.join('/');
+
+				timeArr[0] = timeArr[0] - 1;
+				timeArr[1] = 60 + (timeArr[1] - 10);
+			}
+		}
+		time = `${timeArr[0]}:${timeArr[1]}`;
+		
+		const notifications = await NotificationSchema.find({
+			isSent: false,
+			needToSend: true,
+			time,
+			$or: [
+				{ date: taskDate },
+				{ date: 'everyday' },
+			]
+		});
+		console.log(notifications);
+	} catch (e) {
+		console.log(e);
+	}
 });
