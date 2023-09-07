6b553a71d28bdf9df26929ea8dba77d13de77ce7
NataliiaRybalka <natalirybalka21@gmail.com>
2023-09-07 15:19:20 +0300

    add redux

---
+++ web/src/redux/language.slice.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const languageSlice = createSlice({
+	name: 'language',
+	initialState: {
+		value: 'en',
+	},
+	reducers: {
+		changeLang: (state, action) => {
+			state.value = action.payload
+		}
+	}
+});
+
+export const { changeLang } = languageSlice.actions;
+
+export default languageSlice.reducer;
