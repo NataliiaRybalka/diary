996a6a36d4aa7469ccd6da0e05ccb449d86585fb
NataliiaRybalka <natalirybalka21@gmail.com>
2023-09-07 11:09:16 +0300

    save cards to db via migration

--- server/src/migrations/20230906175000-save-cards.ts
+++ server/src/migrations/20230906175000-save-cards.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import * as csv from 'fast-csv';
 
 import FulcrumSchema from "../db/metaphoricalCards/fulcrum.schema";
 import InternalCompassSchema from "../db/metaphoricalCards/internal-compass.schema";
@@ -8,12 +9,12 @@ const saveFiles = () => {
 	try {
 		const cardsFolderPath = path.join(__dirname, 'lib/cards');
 		fs.readdirSync(cardsFolderPath).forEach((filename: string) => {
-			fs.readFile(path.join(cardsFolderPath, filename), (err, data) => {
+			fs.readFile(path.join(cardsFolderPath, filename), (err, file) => {
 				if (err) throw new Error(err.message);
 				
-				fs.writeFile(path.join('storage', filename), data, (err) => {
+				fs.writeFile(path.join('storage', filename), file, (err) => {
 					if (err) throw new Error(err.message);
-					else return data;
+					else return file;
 				});
 			});
 		});
@@ -24,12 +25,41 @@ const saveFiles = () => {
 };
 
 const saveCardsToDb = async () => {
+	try {
+		const filesPathes = [path.join(__dirname, 'lib/files/fulcrum.csv'), path.join(__dirname, 'lib/files/internalCompass.csv')];
 
+		for (const filePath of filesPathes) {
+			let deckArr = filePath.split('/');
+			deckArr = deckArr[deckArr.length - 1].split('.');
+			const deck = deckArr[0];
+
+			await new Promise<void>((resolve, reject) => {
+				fs.createReadStream(filePath).pipe(
+					csv
+						.parse({headers: true})
+						.on('data', async (row) => {
+							if (!row) return;
+
+							if (deck === 'fulcrum')	await FulcrumSchema.create({ ... row });
+							else if (deck === 'internalCompass') await InternalCompassSchema.create({ ... row });
+						})
+						.on('end', async (rowCount: number) => {
+							resolve();
+						}),
+				);
+			});
+		}
+		
+		console.log('cards was saved');
+	} catch (e: any) {
+		throw new Error(e.message);
+	}
 };
 
 export const up = async () => {
 	try {
 		await saveFiles();
+		await saveCardsToDb();
 	} catch (e) {
 		down(e);
 	}
