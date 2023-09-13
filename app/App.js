c8cb67190b60572152e291f98e56b215e6f680e1
NataliiaRybalka <natalirybalka21@gmail.com>
2023-09-13 14:40:48 +0300

    tring

--- app/App.js
+++ app/App.js
@@ -1,10 +1,26 @@
+import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { FULCRUM, SERVER } from './lib/constants';
+
 export default function App() {
+	const [data, setData] = useState(null);
+
+	const getData = async () => {
+		const res = await fetch(`${SERVER}/metaphorical-cards/${FULCRUM}/card`);
+		const data = await res.json();
+		setData(data.descriptionEn);
+	};
+
+	useEffect(() => {
+		getData();
+	}, []);
+
 	return (
 		<View style={styles.container}>
 			<Text>Open up App.js to start working on your app!</Text>
+			<Text>{data}</Text>
 			<StatusBar style="auto" />
 		</View>
 	);
---
+++ app/lib/constants.js
@@ -0,0 +1,4 @@
+export const FULCRUM='fulcrum';
+export const INTERNAL_COMPASS='internal-compass';
+export const SERVER='https://86a2-185-130-54-83.ngrok-free.app';
+// export const SERVER='http://localhost:4000';
