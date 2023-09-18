528177daaf422472f6eb40f05223af37329d9832
NataliiaRybalka <natalirybalka21@gmail.com>
2023-09-18 15:08:52 +0300

    deleting account not 100%

--- app/components/user/DeletingAccount.js
+++ app/components/user/DeletingAccount.js
@@ -1,16 +1,15 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
+import { View, Text, CheckBox, StyleSheet } from 'react-native';
 
 import { SERVER } from '../../lib/constants';
-
-import './User.css';
 
 function DeletingAccount({user}) {
 	const { t } = useTranslation();
 	const language = useSelector(state => state.language.value);
 
-	const [check, setCheck] = useState();
+	const [check, setCheck] = useState(false);
 	const [err, setErr] = useState(null);
 
 	const deleteAccount = async() => {
@@ -35,18 +34,48 @@ function DeletingAccount({user}) {
 	};
 
 	return (
-		<div className="center">
-			<h2>{t('Delete Account')}</h2>
-			<div className='form'>
-				<div className='checkboxDelete'>
-					<label>{t('Are you sure?')}</label> 
-					<input type='checkbox' name='deleteAccount' value={check} onChange={({target: {checked}}) => setCheck(checked)} />
-				</div>
-				{err && <p className='pError'>{err}</p>}
-				<button className='submit restoreSubmit' onClick={deleteAccount}>{t('Delete')}</button>
-			</div>
-		</div>
+		<View style={[styles.container, { backgroundColor: bgColour }]}>
+			<Text style={styles.label}>Are you sure?</Text>
+			<CheckBox
+				value={check}
+				onValueChange={setCheck}
+				style={styles.checkbox}
+			/>
+			{err && <Text style={styles.err}>{err}</Text>}
+			<View style={styles.btn}>
+				<Text style={styles.btnText} onPress={deleteAccount}>{t('Delete')}</Text>
+			</View>
+		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		textAlign: 'center',
+		fontSize: 16,
+	},
+	err: {
+		color: '#ff0000',
+		textAlign: 'center',
+	},
+	btn: {
+		height: 40,
+		borderRadius: 25,
+		borderColor: '#000000',
+		borderStyle: 'solid',
+		borderWidth: 1,
+		textAlign: 'center',
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: '50%',
+		marginTop: 10,
+		marginLeft: '25%'
+	},
+	btnText: {
+		fontSize: 18,
+		fontWeight: '700',
+	}
+});
 
 export default DeletingAccount;
