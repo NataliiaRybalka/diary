8400e052a917b987a99462bf962d8dd0b2dd18de
NataliiaRybalka <natalirybalka21@gmail.com>
2023-09-18 14:57:29 +0300

    restore password

--- app/components/user/Login.js
+++ app/components/user/Login.js
@@ -134,6 +134,7 @@ const styles = StyleSheet.create({
 		height: 40,
 		margin: 12,
 		borderWidth: 1,
+		borderRadius: 10,
 		padding: 10,
 	},
 	err: {
--- app/components/user/RestoringPassword.js
+++ app/components/user/RestoringPassword.js
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { useSelector } from 'react-redux';
 
 import { SERVER } from '../../lib/constants';
 
 function RestoringPassword() {
 	const { t } = useTranslation();
+	const bgColour = useSelector(state => state.bgColour.value);
 
 	const [email, setEmail] = useState('');
 	const [err, setErr] = useState(null);
@@ -18,19 +20,57 @@ function RestoringPassword() {
 	};
 
 	return (
-		<View>
-			<Text>restorePassword</Text>
-				{/* <div className='form'>
-					<div className="txt_field">
-						<input type="email" name="email" required onChange={e => setEmail(e.target.value)} />
-						<span></span>
-						<label>{t('Email')}</label>
-					</div>
-					{err && <p className='pError'>{err}</p>}
-					<button className='submit restoreSubmit' onClick={restorePassword}>{t('Restore')}</button>
-				</div> */}
+		<View style={[styles.container, { backgroundColor: bgColour }]}>
+			<TextInput
+				textContentType='emailAddress'
+				keyboardType='email-address'
+				style={styles.input} 
+				placeholder='Email'
+				value={email.toString()}
+				onChangeText={text => setEmail(text)}
+			/>
+			{err && <Text style={styles.err}>{err}</Text>}
+			<View style={styles.btn}>
+				<Text style={styles.btnText} onPress={restorePassword}>{t('Restore')}</Text>
+			</View>
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		textAlign: 'center',
+		fontSize: 16,
+	},
+	input: {
+		height: 40,
+		margin: 12,
+		borderWidth: 1,
+		borderRadius: 10,
+		padding: 10,
+	},
+	err: {
+		color: '#ff0000',
+		textAlign: 'center',
+	},
+	btn: {
+		height: 40,
+		borderRadius: 25,
+		borderColor: '#000000',
+		borderStyle: 'solid',
+		borderWidth: 1,
+		textAlign: 'center',
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: '50%',
+		marginTop: 10,
+		marginLeft: '25%'
+	},
+	btnText: {
+		fontSize: 18,
+		fontWeight: '700',
+	}
+});
 
 export default RestoringPassword;
