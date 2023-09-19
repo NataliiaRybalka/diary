ff42741b8e3405168ba26d1cb5dc845cea3415f4
NataliiaRybalka <natalirybalka21@gmail.com>
2023-09-19 18:42:11 +0300

    google login works

--- app/lib/constants.js
+++ app/lib/constants.js
@@ -1,4 +1,4 @@
 export const FULCRUM='fulcrum';
 export const INTERNAL_COMPASS='internal-compass';
-export const SERVER='https://af10-185-130-54-83.ngrok-free.app';
+export const SERVER='https://b697-185-130-54-83.ngrok-free.app';
 // export const SERVER='http://localhost:4000';
