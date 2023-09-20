bfcff3d6c6500cab1de39a6f25948e7bb6b7a47a
NataliiaRybalka <natalirybalka21@gmail.com>
2023-09-20 10:42:24 +0300

    month results

--- app/components/diary/MonthResults.js
+++ app/components/diary/MonthResults.js
@@ -1,37 +1,105 @@
+import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
-import { View, Text, StyleSheet, Linking } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import uuid from 'react-native-uuid';
 
-import { WEB } from '../../lib/constants';
+import { SERVER } from '../../lib/constants';
 
 function MonthResults() {
 	const { t } = useTranslation();
 	const bgColour = useSelector(state => state.bgColour.value);
-	const link = `${WEB}/my-diary/month-results`;
+
+	const fieldsList = [
+		'Date',
+		'Day of the menstrual cycle',
+		'Sleep (h)',
+		'Physical activity',
+		'Drank some water'
+	];
+	const [tableData, setTableData] = useState([]);
+	const [month, setMonth] = useState('');
+
+	useEffect(() => {
+		const date = new Date();
+		const year = date.getFullYear();
+		let month = String(date.getMonth() + 1);
+		month = month.length === 1 ? `0${month}` : month;
+		setMonth(`${year}-${month}`);
+	}, []);
+
+	useEffect(() => {
+		getMonthResult();
+	}, [month]);
+
+	const getMonthResult = async () => {
+		const user = await AsyncStorage.getItem('user');
+		const res = await fetch(`${SERVER}/diary/result/${JSON.parse(user).id}/${month}`);
+
+		const data = await res.json();
+		setTableData(data);
+	};
 
 	return (
-		<View style={[styles.container, {backgroundColor: bgColour}]}>
-			<Text style={styles.text}>{t('You can see your results on the website')}:</Text>
-			<Text style={styles.link} onPress={() => Linking.openURL(link)}>{t('Month Results')}</Text>
+		<ScrollView style={[styles.container, {backgroundColor: bgColour}]}>
+		<View style={styles.table}>
+			<View style={styles.row}>
+				{fieldsList.map((field, index) => (
+					<Text style={styles.cellHeader} key={`${uuid.v4()}${index}`}>{t(field)}</Text>
+				))}
+			</View>
+			
+			{tableData.length 
+				? tableData.map((row, rowI) => (
+					<View style={styles.row}>
+						<Text style={styles.cell} key={`${uuid.v4()}${rowI}`}>{tableData[rowI].date}</Text>
+						<Text style={styles.cell} key={`${uuid.v4()}${rowI}`}>{tableData[rowI].menstrualDay}</Text>
+						<Text style={styles.cell} key={`${uuid.v4()}${rowI}`}>{tableData[rowI].totalHours}</Text>
+						<Text style={styles.cell} key={`${uuid.v4()}${rowI}`}>{tableData[rowI].physicalActivity}</Text>
+						<Text style={styles.cell} key={`${uuid.v4()}${rowI}`}>{tableData[rowI].drankWater}</Text>
+					</View>
+				))
+				: <></>
+			}
 		</View>
+		</ScrollView>
 	);
 };
 
 const styles = StyleSheet.create({
 	container: {
-		height: '100%',
+		minHeight: '100%',
+	},
+	table: {
+		borderWidth: 1,
+		borderColor: '#000000',
+		marginBottom: 10,
+		marginTop: 30,
+	},
+	row: {
+		flexDirection: 'row',
 		justifyContent: 'center',
+		alignItems: 'center',
 	},
-	text: {
+	cellHeader: {
+		flex: 1,
+		borderWidth: 1,
+		height: 50,
 		textAlign: 'center',
-		fontSize: 20,
-		fontWeight: '500'
+		fontSize: 12,
+		fontWeight: '500',
+		borderColor: '#000000',
 	},
-	link: {
+	cell: {
+		flex: 1,
+		padding: 10,
+		borderWidth: 1,
+		height: 50,
 		textAlign: 'center',
-		fontSize: 20,
-		color: 'blue'
-	}
+		fontSize: 12,
+		borderColor: '#000000',
+	},
 });
 
 export default MonthResults;
--- app/lib/constants.js
+++ app/lib/constants.js
@@ -1,5 +1,5 @@
 export const FULCRUM='fulcrum';
 export const INTERNAL_COMPASS='internal-compass';
-export const SERVER='https://91ac-185-130-54-83.ngrok-free.app';
+export const SERVER='https://9cb7-185-130-54-83.ngrok-free.app';
 // export const SERVER='http://localhost:4000';
 export const WEB='http://localhost:3000';
