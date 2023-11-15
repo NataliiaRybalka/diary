58ddb0ead237c12d8e3b699a0f65084076deffcd
NataliiaRybalka <natalirybalka21@gmail.com>
2023-11-15 10:24:34 +0200

    add email vew

--- web/src/components/pages/Footer.js
+++ web/src/components/pages/Footer.js
@@ -1,20 +1,33 @@
+import { useTranslation } from 'react-i18next';
+
 import './Pages.css';
 
 function Footer() {
+	const { t } = useTranslation();
 
 	return (
 		<footer>
 			<div>
 				<p className={'footerLink'}>
-				<span>
-					&copy; 2023 - {new Date().getFullYear()}{" "}
-					<a
-					href="https://github.com/NataliiaRybalka"
-					target="blank"
-					>
-						<span className='developerName'>Nataliia Rybalka</span>
-					</a>
-				</span>
+					<span>
+						{t('For any questions: ')}
+						<span
+							className='developerEmail'
+							onClick={() => window.location = 'mailto:your.best.friend.diary@gmail.com'}
+						>
+							your.best.friend.diary@gmail.com
+						</span>
+					</span>
+
+					<span>
+						&copy; 2023 - {new Date().getFullYear()}{" "}
+						<a
+						href="https://github.com/NataliiaRybalka"
+						target="blank"
+						>
+							<span className='developerName'>Nataliia Rybalka</span>
+						</a>
+					</span>
 				</p>
 			</div>
 		</footer>
