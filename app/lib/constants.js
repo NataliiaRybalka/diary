7a008778300d66907bafcfd59db2a63c95780550
NataliiaRybalka <natalirybalka21@gmail.com>
2023-11-23 21:02:19 +0200

    update

--- app/lib/constants.js
+++ app/lib/constants.js
@@ -1,3 +1,4 @@
 export const FULCRUM='fulcrum';
 export const INTERNAL_COMPASS='internal-compass';
-export const SERVER='http://184.73.121.241:4000';
+export const SERVER='https://yourbestfriend.website/server';
+// export const SERVER='http://184.73.121.241:4000';
